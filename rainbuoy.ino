54c1d861f0dbaa6a3e7eb7d6d4b78989a253c7e9
EeroVakiparta <eero.vakiparta@student.samk.fi>
2021-05-09 22:38:51 +0300

    Some tests

--- rainbuoy.ino
+++ rainbuoy.ino
@@ -3,24 +3,34 @@
   #include <avr/power.h>
 #endif
 
-#define PIN 6
+#define LEDS_PIN 6
+#define POT_PIN A0
 #define STRIPLEDCOUNT 30
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(STRIPLEDCOUNT, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(STRIPLEDCOUNT, LEDS_PIN, NEO_GRB + NEO_KHZ800);
+
+int potValue = 0;
+int lednumber = 0;
+int i, n;
 
 void setup() {
   strip.begin();
   strip.setBrightness(50);
   strip.show(); 
+  Serial.begin(9600);
 }
 
 void loop() {
- int i, n;
-  for(i=1; i<=STRIPLEDCOUNT; i++)
-    {
-        if(i%2!=0)
-        {
-            strip.setPixelColor(i,strip.Color(255, 0, 0));
-        }
-    }
-    strip.show();
+  potValue = analogRead(POT_PIN);
+  lednumber = map(lednumber, 0, 1023, 0, STRIPLEDCOUNT);
+
+  Serial.print("potValue = ");
+  Serial.print(potValue);
+  Serial.print("\t lednumber = ");
+  Serial.println(lednumber);
+  
+for(i=1; i<=lednumber; i++){
+    strip.setPixelColor(i,strip.Color(255, 0, 0));
+  }
+  strip.show();
+  delay(50);
 }
